f9ff5315055afe59dda7158fe1b281622a3590c7
ClassHacker <Ajaydayma1401@gmail.com>
2022-11-23 00:32:22 +0530

    Added a test case

---
+++ tic-tac-toe/src/index.test.js
@@ -0,0 +1,9 @@
+// import { render, screen } from '@testing-library/react';
+// import Home from './sound';
+
+test('renders Home page', () => {
+  expect(1).toBe(1);
+  // render(<Home root={"root"}/>);
+  // const linkElement = screen.getByText(/Loading.../i);
+  // expect(linkElement).toBeInTheDocument();
+});
