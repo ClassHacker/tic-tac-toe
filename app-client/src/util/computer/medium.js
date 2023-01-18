b688fa78f3e8450a4e7567e9063ae04554f8f748
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-18 18:06:26 +0000

    added code logic for third move

--- app-client/src/util/computer/medium.js
+++ app-client/src/util/computer/medium.js
@@ -21,8 +21,31 @@ export class Computer extends SuperComputer {
     
     makeThirdMove(squares, i) {
         this.X[2] = i;
-        this.O[2] = this.getRandomMove();
-        return this.O[2];
+
+        // if O is winning
+        let steps = String(Math.min(this.O[0], this.O[1])) + String(Math.max(this.O[0], this.O[1]));
+        let index = this.getMoveIndex(steps);
+        if (index != null && this.X.indexOf(index) === -1) {
+            this.O[2] = index;
+            return this.O[2];
+        }
+        // if X is winning
+        steps = String(Math.min(this.X[0], this.X[2])) + String(Math.max(this.X[0], this.X[2]));
+        index = this.getMoveIndex(steps);
+        if (index != null && !(this.O.includes(index) || this.X.includes(index))) {
+            this.O[2] = index;
+            return this.O[2];
+        }
+        // if x is winning
+        steps = String(Math.min(this.X[1], this.X[2])) + String(Math.max(this.X[1], this.X[2]));
+        index = this.getMoveIndex(steps);
+        if (index != null && this.O.indexOf(index) === -1 && this.X.indexOf(index) === -1) {
+            this.O[2] = index;
+            return this.O[2];
+        }
+        // nobody is winning
+        this.O[3] = this.getRandomMove(); 
+        return this.O[3];
     }
     
     makeFourthMove(squares, i) {
