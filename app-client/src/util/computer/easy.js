deee8d49eb6966fa9a747eb1b9b58d2dd839e2b5
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-18 17:31:50 +0000

    removed useless constructor

--- app-client/src/util/computer/easy.js
+++ app-client/src/util/computer/easy.js
@@ -1,10 +1,6 @@
 import { SuperComputer } from "./superComputer";
 
 export class Computer extends SuperComputer {
-    constructor() {
-        super();
-    }
-
     makeFirstMove(squares, i) {
         this.X[0] = i;
         this.O[0] = this.getRandomMove();
