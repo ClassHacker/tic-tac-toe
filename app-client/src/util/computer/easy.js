98ec2cdf576120175d8d54cdae0a2462ed64c767
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-19 18:29:37 +0000

    easy level: added logic for some moves

--- app-client/src/util/computer/easy.js
+++ app-client/src/util/computer/easy.js
@@ -1,6 +1,7 @@
 import { SuperComputer } from "./superComputer";
 
 export class Computer extends SuperComputer {
+    
     makeFirstMove(squares, i) {
         this.X[0] = i;
         this.O[0] = this.getRandomMove();
@@ -9,14 +10,43 @@ export class Computer extends SuperComputer {
     
     makeSecondMove(squares, i) {
         this.X[1] = i;
-        this.O[1] = this.getRandomMove();;
+
+        // if X is winning
+        let steps = String(Math.min(this.X[0], this.X[1])) + String(Math.max(this.X[0], this.X[1]));
+        this.O[1] = this.getMoveIndex(steps);
+        // if X is not winning
+        if(!this.O[1] || this.O[1] === this.O[0])
+            this.O[1] = this.getRandomMove();
         return this.O[1];
     }
     
     makeThirdMove(squares, i) {
         this.X[2] = i;
-        this.O[2] = this.getRandomMove();
-        return this.O[2];
+
+        // if O is winning
+        let steps = String(Math.min(this.O[0], this.O[1])) + String(Math.max(this.O[0], this.O[1]));
+        let index = this.getMoveIndex(steps);
+        if (index != null && this.X.indexOf(index) === -1) {
+            this.O[2] = index;
+            return this.O[2];
+        }
+        // if X is winning
+        steps = String(Math.min(this.X[0], this.X[2])) + String(Math.max(this.X[0], this.X[2]));
+        index = this.getMoveIndex(steps);
+        if (index != null && !(this.O.includes(index) || this.X.includes(index))) {
+            this.O[2] = index;
+            return this.O[2];
+        }
+        // if x is winning
+        steps = String(Math.min(this.X[1], this.X[2])) + String(Math.max(this.X[1], this.X[2]));
+        index = this.getMoveIndex(steps);
+        if (index != null && this.O.indexOf(index) === -1 && this.X.indexOf(index) === -1) {
+            this.O[2] = index;
+            return this.O[2];
+        }
+        // nobody is winning
+        this.O[3] = this.getRandomMove(); 
+        return this.O[3];
     }
     
     makeFourthMove(squares, i) {
