27993d3c73ad2659cf0d85ad39c0d185d8624dee
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-26 13:24:31 +0530

    medium level: use updated fourth move

--- app-client/src/util/computer/medium.js
+++ app-client/src/util/computer/medium.js
@@ -51,9 +51,7 @@ export class Computer extends SuperComputer {
         return this.O[3];
     }
     
-//     makeFourthMove(squares, i) {
-//         this.X[3] = i;
-//         this.O[3] = this.getRandomMove(); 
-//         return this.O[3];
-//     }
+    makeFourthMove(squares, i) {
+        return this.makeFourthMoveV2(squares, i);
+    }
 }
