a2c15837627d5576425fd5cdba862d696b1f3bd2
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-28 14:35:15 +0000

    added message model

---
+++ tic-tac-toe-api/src/msgModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const msgModel = new mongoose.Schema({
+    _id: Number,
+    name: {
+        type: String,
+    },
+    message : {
+        type: String,
+    }
+})
+
+
+const Message = mongoose.model("msg",msgModel);
+
+module.exports = Message
