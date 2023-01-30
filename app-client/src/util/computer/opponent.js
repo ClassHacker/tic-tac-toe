291fd10c3036b5d0f370a3e4acebaff7e2676204
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-30 17:26:33 +0000

    use uppercase for levels

--- app-client/src/util/computer/opponent.js
+++ app-client/src/util/computer/opponent.js
@@ -4,12 +4,13 @@ const MediumLevelOpponent = require('./medium');
 const GodLevelOpponent = require('./superComputer');
 
 export function getOpponent(level) {
-  console.log("Opponent level: ", level.length? level.toUpperCase: "MEDIUM")  
-  switch (level) {
-    case 'easy': return new EasyLevelOpponent.Computer();
-    case 'medium': return new MediumLevelOpponent.Computer();
-    case 'hard': return new HardLevelOpponent.Computer();
-    case 'god': return new GodLevelOpponent.Computer();
+  level = ""
+  console.log("Opponent level: ", level.length? level.toUpperCase(): "MEDIUM")  
+  switch (level.toUpperCase()) {
+    case 'EASY': return new EasyLevelOpponent.Computer();
+    case 'MEDIUM': return new MediumLevelOpponent.Computer();
+    case 'HARD': return new HardLevelOpponent.Computer();
+    case 'GOD': return new GodLevelOpponent.Computer();
     default: return new MediumLevelOpponent.Computer();
   }
 }
