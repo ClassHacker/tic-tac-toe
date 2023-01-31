1a5143e646d4768da47b5e269e9a88f7e6b2f8ca
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-01-31 17:34:21 +0000

    Add container

--- app-client/src/component/modals/level.js
+++ app-client/src/component/modals/level.js
@@ -1,27 +1,30 @@
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, Row, Col, Container } from "react-bootstrap";
 
 export function Level(props) {
     const { setLevel, show } = props;
     return (
         <Modal
         show={show}
-        centered>
+        centered
+        size={'sm'}>
             <Modal.Header>
                 <h2>Please select a level</h2>
             </Modal.Header>
-            <Modal.Body>
-                <Button variant="primary" size="" className="min-w" onClick={() => setLevel('EASY')}>
-                Easy Level
-                </Button>{' '}
-                <Button variant="primary" className="min-w" onClick={() => setLevel('MEDIUM')}>
-                Medium Level
-                </Button>{' '}
-                <Button variant="primary" className="min-w" onClick={() => setLevel('HARD')}>
-                Hard Level
-                </Button>{' '}
-                <Button variant="primary" className="min-w" onClick={() => setLevel('GOD')}>
-                God Level
-                </Button>
+            <Modal.Body >
+                <Container>
+                    <Button variant="primary" className="min-w" onClick={() => setLevel('EASY')}>
+                        Easy Level
+                    </Button>
+                    <Button variant="primary" className="min-w" onClick={() => setLevel('MEDIUM')}>
+                        Medium Level
+                    </Button>
+                    <Button variant="primary" className="min-w" onClick={() => setLevel('HARD')}>
+                        Hard Level
+                    </Button>
+                    <Button variant="primary" className="min-w" onClick={() => setLevel('GOD')}>
+                        God Level
+                    </Button>
+                </Container>
             </Modal.Body>
         </Modal>
     )
