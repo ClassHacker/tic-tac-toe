7855e51b3045d725a15e55f1980c1598aa4a2104
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-02-05 19:12:30 +0000

    hard level: code rafactored

--- app-client/src/util/computer/hard.js
+++ app-client/src/util/computer/hard.js
@@ -1,7 +1,6 @@
 import { SuperComputer } from "./superComputer";
 
 export class Computer extends SuperComputer {
-    
     makeFourthMove(squares, i) {
         this.X[3] = i;
         this.O[3] = this.getRandomMove();
