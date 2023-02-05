70a3d77baf6208b7048ed3fef5533cb729228c5a
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-02-05 19:06:32 +0000

    hard level: make fourth move random

--- app-client/src/util/computer/hard.js
+++ app-client/src/util/computer/hard.js
@@ -22,10 +22,10 @@ export class Computer extends SuperComputer {
         return this.O[1];
     }
     
-    // makeFourthMove(squares, i) {
-    //     this.X[3] = i;
-    //     this.O[3] = this.getRandomMove();
-    //     console.log('fourth move index:', this.O[3]);
-    //     return this.O[3];
-    // }
+    makeFourthMove(squares, i) {
+        this.X[3] = i;
+        this.O[3] = this.getRandomMove();
+        console.log('fourth move index:', this.O[3]);
+        return this.O[3];
+    }
 }
