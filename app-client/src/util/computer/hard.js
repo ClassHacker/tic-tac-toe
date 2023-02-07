677fb99859a21fa0f8a7a6f320feddddf72d94dc
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-02-07 15:43:06 +0000

    hard level: update fourth move

--- app-client/src/util/computer/hard.js
+++ app-client/src/util/computer/hard.js
@@ -1,9 +1,17 @@
 import { SuperComputer } from "./superComputer";
 
 export class Computer extends SuperComputer {
-    makeFourthMove(squares, i) {
-        this.X[3] = i;
-        this.O[3] = this.getRandomMove();
+    makeFourthMove(squares, ind) {
+        this.X[3] = ind;
+        let indexes = [];
+        for(let i = 0; i < 9; i++) {
+            if (this.X.indexOf(i) === -1 && this.O.indexOf(i) === -1) {
+                indexes.push(i);
+            }
+        }
+        this.O[3] = this.getLastMoveIndex(indexes);
+        // indexes.push(this.O[3]);
+        // this.O[3] = this.getRandom(indexes);
         console.log('fourth move index:', this.O[3]);
         return this.O[3];
     }
