0ba4b0ae2a011bf71e9d4adc97d6cfe419908449
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-02-07 15:44:59 +0000

    hard level: optimize fourth move

--- app-client/src/util/computer/hard.js
+++ app-client/src/util/computer/hard.js
@@ -10,7 +10,8 @@ export class Computer extends SuperComputer {
             }
         }
         this.O[3] = this.getLastMoveIndex(indexes);
-        // indexes.push(this.O[3]);
+        indexes.push(this.O[3]);
+        indexes.push(this.O[3]);
         this.O[3] = this.getRandom(indexes);
         console.log('fourth move index:', this.O[3]);
         return this.O[3];
