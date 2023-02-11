2586945a106ea22c6f8f095eeee147db744d4075
classhacker <ajaydayma1401@gmail.com>
2023-02-11 21:10:35 +0530

    update test case

--- app-client/src/index.test.js
+++ app-client/src/index.test.js
@@ -1,7 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './app';
 
 test('renders App', () => {
-  expect(1).toBe(1);
   render(<App root={"root"}/>);
 });
