d67747d83d358bd7aaf07c7afddeaaa0297dd864
classhacker <ajaydayma1401@gmail.com>
2023-02-12 20:24:55 +0530

    exit modal: update test case

--- app-client/src/component/modals/exit.test.js
+++ app-client/src/component/modals/exit.test.js
@@ -1,12 +1,11 @@
+import { screen } from "@testing-library/react";
 import { ExitModal } from "./exit";
 import { renderWithProviders } from "../../utils/test/test-utils";
 
 describe('test exitModal', ()=> {
     it('should render exitModal', ()=> {
-        renderWithProviders(<ExitModal />, {
-        preloadedState: {
-            exR: true
-          }
-        });
+        renderWithProviders(<ExitModal />, { preloadedState: {exR: true} });
+        const modalBodyText = screen.getByText(/Do you really want to exit the game?/i);
+        expect(modalBodyText).toBeInTheDocument();
     })
 })
