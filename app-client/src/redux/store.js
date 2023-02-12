b606afaf9a1908ff0f104ac57720d0e286007e6a
classhacker <ajaydayma1401@gmail.com>
2023-02-12 19:32:13 +0530

    use configureStore

--- app-client/src/redux/store.js
+++ app-client/src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 import reducer from "./reducers";
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store2 = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = configureStore({reducer: reducer});
 store.subscribe(() => console.log(store.getState()));
 // store.dispatch(action)
 
