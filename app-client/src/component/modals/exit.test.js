e398f5c3e2cecd46727ea519633343463d43a6f5
classhacker <ajaydayma1401@gmail.com>
2023-02-12 20:49:06 +0530

    exit modal: add test case for yes button

--- app-client/src/component/modals/exit.test.js
+++ app-client/src/component/modals/exit.test.js
@@ -1,11 +1,17 @@
-import { screen } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import { ExitModal } from "./exit";
 import { renderWithProviders } from "../../utils/test/test-utils";
 
 describe('test exitModal', ()=> {
-    it('should render exitModal', ()=> {
+    const exitFunc = jest.fn()
+    it('should render exitModal', () => {
         renderWithProviders(<ExitModal />, { preloadedState: {exR: true} });
         const modalBodyText = screen.getByText(/Do you really want to exit the game?/i);
         expect(modalBodyText).toBeInTheDocument();
+    })
+    it('should exit game on Yes', () => {
+        renderWithProviders(<ExitModal exitGame={exitFunc}/>, { preloadedState: {exR: true} });
+        fireEvent.click(screen.getByText(/Yes/i));
+        expect(exitFunc).toHaveBeenCalledTimes(1);
     })
 })
