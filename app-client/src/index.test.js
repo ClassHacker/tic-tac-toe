edd8eb6c0f866de27a02b1d058f8c1d713175b16
Ajay Dayma <60310120+ClassHacker@users.noreply.github.com>
2023-02-13 19:35:28 +0000

    code coverage: add test case

--- app-client/src/index.test.js
+++ app-client/src/index.test.js
@@ -1,10 +1,31 @@
-import { render } from '@testing-library/react';
-import App from './app';
+// import { render } from '@testing-library/react';
+// import App from './app';
 // import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
-// const render = jest.fn().mockName('render')
+import { renderWithProviders } from "./utils/test/test-utils";
 
-it('should render without crashing', () => {
-  render(<App root={"root"}/>);
-});
+// const render = jest.fn().mockName('render');
+const render = renderWithProviders();
+
+jest.mock('react');
+jest.mock("react-dom/client", () => ({ createRoot: jest.fn().mockName('createRoot') }));
+let documentSpy = jest.spyOn(document, 'getElementById')
+
+describe('Application root', () => {
+  it("shoudl render without crashing", () =>{
+    const div = document.createElement("div");
+    div.setAttribute("id", "root");
+
+    ReactDOM.createRoot.mockReturnValue({render});
+    require("./index.js");
+
+    expect(ReactDOM.createRoot).toHaveBeenCalledTimes(1);
+    expect(documentSpy).toHaveBeenCalledTimes(1);
+  })
+})
+
+
+// it('should render without crashing', () => {
+//   render(<App root={"root"}/>);
+// });
