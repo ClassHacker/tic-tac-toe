aa22a19c81b6730104cc643fb974ed8d8cb7de87
classhacker <ajaydayma1401@gmail.com>
2023-02-13 23:35:01 +0530

    add test case for code coverage

--- app-client/src/component/modals/exit.test.js
+++ app-client/src/component/modals/exit.test.js
@@ -3,15 +3,21 @@ import { ExitModal } from "./exit";
 import { renderWithProviders } from "../../utils/test/test-utils";
 
 describe('test exitModal', ()=> {
-    const exitFunc = jest.fn()
     it('should render exitModal', () => {
         renderWithProviders(<ExitModal />, { preloadedState: {exR: true} });
         const modalBodyText = screen.getByText(/Do you really want to exit the game?/i);
         expect(modalBodyText).toBeInTheDocument();
     })
     it('should exit game on Yes', () => {
+        const exitFunc = jest.fn()
         renderWithProviders(<ExitModal exitGame={exitFunc}/>, { preloadedState: {exR: true} });
         fireEvent.click(screen.getByText(/Yes/i));
         expect(exitFunc).toHaveBeenCalledTimes(1);
+    })
+    it('should not exit game on No', () => {
+        const exitFunc = jest.fn()
+        renderWithProviders(<ExitModal exitGame={exitFunc}/>, { preloadedState: {exR: true} });
+        fireEvent.click(screen.getByText(/No/i));
+        expect(exitFunc).toHaveBeenCalledTimes(0);
     })
 })
