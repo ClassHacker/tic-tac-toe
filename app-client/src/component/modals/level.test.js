34f86295aeea4e288dbdbcc0e37796960999332d
classhacker <ajaydayma1401@gmail.com>
2023-02-14 13:50:15 +0530

    jest: use beforeEach & afterEach

--- app-client/src/component/modals/level.test.js
+++ app-client/src/component/modals/level.test.js
@@ -2,8 +2,14 @@ import { screen, fireEvent, render } from "@testing-library/react";
 import { Level } from "./level";
 
 describe('test level modal', () => {
+    let setLevelMock = jest.fn();
+    beforeEach(() => {
+        render(<Level setLevel = {setLevelMock} show={true} />);
+    })
+    afterEach(() => {
+        setLevelMock.mockReset();
+    })
     it('should show modal with all buttons', () => {
-        render(<Level setLevel = {null} show={true} />);
         const heading = screen.queryByText(/Please select a level/i);
         expect(heading).toBeInTheDocument();
         const btn1 = screen.queryByText(/Easy Level/i);
@@ -17,31 +23,27 @@ describe('test level modal', () => {
         expect(btn4).toBeTruthy();
     })
     it('should select EASY level', () => {
-        const setLevelMock = jest.fn();
-        render(<Level setLevel = {setLevelMock} show={true} />);
         const btn1 = screen.queryByText(/Easy Level/i);
         fireEvent.click(btn1);
         expect(setLevelMock).toHaveBeenCalledWith('EASY');
+        expect(setLevelMock).toHaveBeenCalledTimes(1);
     })
     it('should select MEDIUM level', () => {
-        const setLevelMock = jest.fn();
-        render(<Level setLevel = {setLevelMock} show={true} />);
         const btn1 = screen.queryByText(/MEDIUM Level/i);
         fireEvent.click(btn1);
         expect(setLevelMock).toHaveBeenCalledWith('MEDIUM');
+        expect(setLevelMock).toHaveBeenCalledTimes(1);
     })
     it('should select HARD level', () => {
-        const setLevelMock = jest.fn();
-        render(<Level setLevel = {setLevelMock} show={true} />);
         const btn1 = screen.queryByText(/HARD Level/i);
         fireEvent.click(btn1);
         expect(setLevelMock).toHaveBeenCalledWith('HARD');
+        expect(setLevelMock).toHaveBeenCalledTimes(1);
     })
     it('should select GOD level', () => {
-        const setLevelMock = jest.fn();
-        render(<Level setLevel = {setLevelMock} show={true} />);
         const btn1 = screen.queryByText(/GOD Level/i);
         fireEvent.click(btn1);
         expect(setLevelMock).toHaveBeenCalledWith('GOD');
+        expect(setLevelMock).toHaveBeenCalledTimes(1);
     })
 })
