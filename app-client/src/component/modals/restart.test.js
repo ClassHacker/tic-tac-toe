549ac15def841dc746b83cdd45141588117b0ca5
classhacker <ajaydayma1401@gmail.com>
2023-02-14 14:15:14 +0530

    restart modal: updated test cases

--- app-client/src/component/modals/restart.test.js
+++ app-client/src/component/modals/restart.test.js
@@ -3,21 +3,27 @@ import { renderWithProviders } from "../../utils/test/test-utils";
 import { RestartModal } from "./restart";
 
 describe('test restart modal', ()=> {
-    it('should render exitModal', () => {
-        renderWithProviders(<RestartModal />, { preloadedState: {rsR: true} });
-        const modalBodyText = screen.getByText(/Do you really want to restart the game?/i);
-        expect(modalBodyText).toBeInTheDocument();
+    const restartGameMock = jest.fn();
+    beforeEach(() => {
+        renderWithProviders(<RestartModal restartGame={restartGameMock} winner={'winner'}/>, 
+            { preloadedState: {rsR: true} });
     })
-    it('should exit game on Yes', () => {
-        const restartGameMock = jest.fn()
-        renderWithProviders(<RestartModal restartGame={restartGameMock} winner={'winner'}/>, { preloadedState: {rsR: true} });
+    afterEach(() => {
+        restartGameMock.mockReset();
+    })
+    it('should show modal with both buttons', () => {
+        const modalBodyText = screen.getByText(/Do you really want to restart the game?/i);
+        const btn1 = screen.queryByText(/Yes/i);
+        const btn2 = screen.queryByText(/No/i);
+        expect(modalBodyText).toBeTruthy();
+        expect(btn1).toBeTruthy();
+        expect(btn2).toBeInTheDocument();
+    })
+    it('should restart game on Yes', () => {
         fireEvent.click(screen.getByText(/Yes/i));
         expect(restartGameMock).toHaveBeenCalledWith("winner");
-        expect(restartGameMock).toHaveBeenCalledTimes(1);
     })
-    it('should not exit game on No', () => {
-        const restartGameMock = jest.fn()
-        renderWithProviders(<RestartModal restartGame={restartGameMock}/>, { preloadedState: {rsR: true} });
+    it('should not restart game on No', () => {
         fireEvent.click(screen.getByText(/No/i));
         expect(restartGameMock).toHaveBeenCalledTimes(0);
     })
