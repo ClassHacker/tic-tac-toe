71bcc07d3add4ea641fe334039e07868fbba73c7
classhacker <ajaydayma1401@gmail.com>
2023-02-15 23:13:00 +0530

    coverage: added test case

--- app-client/src/component/scoreCard.test.js
+++ app-client/src/component/scoreCard.test.js
@@ -3,9 +3,15 @@ import { ScoreCard } from "./scoreCard";
 
 describe('test score card', () => {
     const players = [{name:'user', gamesWon: 2},{name:'computer', gamesWon: 1}];
-    render(<ScoreCard players={players} />);
-    it("should show score", () => {
+    beforeEach(() => {
+        render(<ScoreCard players={players} />);
+    })
+    it("should show user's score", () => {
         const userScore = screen.queryByText(/user : 2/i)
         expect(userScore).toBeTruthy();
+    })
+    it("should show computer's score", () => {
+        const computerScore = screen.queryByText(/user : 2/i)
+        expect(computerScore).toBeTruthy();
     })
 })
