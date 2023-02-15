951164e44f2f4d2ffc49f70d30a71983cca2d292
classhacker <ajaydayma1401@gmail.com>
2023-02-15 23:49:25 +0530

    added test case for board

---
+++ app-client/src/component/board.test.js
@@ -0,0 +1,10 @@
+import { screen, render , fireEvent} from "@testing-library/react";
+import Board from "./board";
+
+describe("test board", () => {
+    it('should render game board', () => {
+        const onClickMock = jest.fn();
+        const squares = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        render(<Board onClick={onClickMock} squares={squares}/>);
+    })
+})
