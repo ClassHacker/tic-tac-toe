59924e7e2c133b3c6ae93a9530c7a3eb1d5dca26
classhacker <ajaydayma1401@gmail.com>
2023-02-17 16:24:26 +0530

    covered 1 scenario

--- app-client/src/utils/computer/easy.test.js
+++ app-client/src/utils/computer/easy.test.js
@@ -1,10 +1,24 @@
-const EasyLevelOpponent = require('./easy');
+// const EasyLevelOpponent = require('./easy');
+import { Computer as EasyLevelOpponent } from './easy';
 
+const sideMoveMock = jest.spyOn(EasyLevelOpponent.prototype, 'getRandomSideMove')
+                        .mockImplementation(() => {
+                            return 1;
+                        })
+const cornerMoveMock = jest.spyOn(EasyLevelOpponent.prototype, 'getRandomCornerMove')
+                        .mockImplementation(() => {
+                            return 6;
+                        })
+const randomMoveMock = jest.spyOn(EasyLevelOpponent.prototype, 'getRandomMove')
+                        .mockImplementation(() => {
+                            return 8;
+                        })
 describe("test easy level opponent", () => {
-    let opponent = new EasyLevelOpponent.Computer('EASY');
+    let opponent = new EasyLevelOpponent('EASY');
     it('should return moves', () => {
         expect(opponent.makeFirstMove([], 0)).toBeGreaterThan(0);
-        expect(opponent.makeSecondMove([], 2)).toBeGreaterThan(0);
+        expect(sideMoveMock).toHaveBeenCalled();
+        expect(opponent.makeSecondMove([], 3)).toBeGreaterThan(0);
         expect(opponent.makeThirdMove([], 8)).toBeGreaterThan(0);
     })
 })
