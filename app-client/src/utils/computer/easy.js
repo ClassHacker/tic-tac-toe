40e023e09ca5e4cdd368b7219b151976fe0ab931
classhacker <ajaydayma1401@gmail.com>
2023-02-17 16:09:32 +0530

    easy level: add more logs

--- app-client/src/utils/computer/easy.js
+++ app-client/src/utils/computer/easy.js
@@ -36,6 +36,7 @@ export class Computer extends SuperComputer {
         let index = this.getMoveIndex(steps);
         if (index != null && this.X.indexOf(index) === -1) {
             this.O[2] = index;
+            console.log('third move index:', this.O[2]);
             return this.O[2];
         }
         // if X is winning
@@ -43,6 +44,7 @@ export class Computer extends SuperComputer {
         index = this.getMoveIndex(steps);
         if (index != null && !(this.O.includes(index) || this.X.includes(index))) {
             this.O[2] = index;
+            console.log('third move index:', this.O[2]);
             return this.O[2];
         }
         // if x is winning
@@ -50,6 +52,7 @@ export class Computer extends SuperComputer {
         index = this.getMoveIndex(steps);
         if (index != null && this.O.indexOf(index) === -1 && this.X.indexOf(index) === -1) {
             this.O[2] = index;
+            console.log('third move index:', this.O[2]);
             return this.O[2];
         }
         // nobody is winning
