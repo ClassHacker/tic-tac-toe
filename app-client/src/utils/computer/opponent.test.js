cd41a34532f252481c194bd228e9aae6e8dfcf63
classhacker <ajaydayma1401@gmail.com>
2023-02-17 15:41:35 +0530

    updated test case

--- app-client/src/utils/computer/opponent.test.js
+++ app-client/src/utils/computer/opponent.test.js
@@ -1,10 +1,10 @@
 import { getOpponent } from "./opponent";
 
 describe("test getOpponent", () => {
-    it('should return correct opponent', () => {
-        expect(getOpponent('EASY').level).toBe('EASY');
+    it('should return opponent correct level', () => {
+        expect(getOpponent('easy').level).toBe('EASY');
         expect(getOpponent('MEDIUM').level).toBe('MEDIUM');
-        expect(getOpponent('HARD').level).toBe('HARD');
+        expect(getOpponent('hARD').level).toBe('HARD');
         expect(getOpponent('GOD').level).toBe('GOD');
         expect(getOpponent('').level).toBe('MEDIUM');
     })
