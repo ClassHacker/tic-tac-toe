b5711a2ab54cc36800d2699f51905abb2ca6a912
classhacker <ajaydayma1401@gmail.com>
2023-02-21 10:11:13 +0530

    add key for animation

--- app-client/src/component/square.js
+++ app-client/src/component/square.js
@@ -3,6 +3,7 @@ import './square.scss';
 function Square(props) {
     return (
         <button
+          key={props.value}
           className="square zoomIn" 
           onClick={props.onClick}
         >
