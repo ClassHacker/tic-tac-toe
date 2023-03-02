70b5d610bc07ac6fabc2f5d47d06d9748e7bf7f5
classhacker <ajaydayma1401@gmail.com>
2023-03-02 11:01:37 +0530

    build fix

--- app-client/src/utils/sound.js
+++ app-client/src/utils/sound.js
@@ -1,22 +1,22 @@
-import backGround from "../common/bg.mp3";
-import button1 from "../common/b1.wav";
-import button2 from "../common/b2.wav";
+// import backGround from "../common/bg.mp3";
+// import button1 from "../common/b1.wav";
+// import button2 from "../common/b2.wav";
  
 export function playSound(sound) {
-    let audioName = "";
-    switch(sound) {
-        case "b1":
-            audioName = button1;
-            break;
-        case "b2":
-            audioName = button2;
-            break;
-        case 'bg':
-            audioName = backGround;
-            break;
-        default:
-            audioName = '';
-    }
+    // let audioName = "";
+    // switch(sound) {
+    //     case "b1":
+    //         audioName = button1;
+    //         break;
+    //     case "b2":
+    //         audioName = button2;
+    //         break;
+    //     case 'bg':
+    //         audioName = backGround;
+    //         break;
+    //     default:
+    //         audioName = '';
+    // }
     // var promise = 
     document.getElementById(sound).play();
 
