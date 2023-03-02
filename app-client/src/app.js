a68c274205b37afaadff3203bc4c15c42a233bea
classhacker <ajaydayma1401@gmail.com>
2023-03-02 10:45:52 +0530

    add audio element

--- app-client/src/app.js
+++ app-client/src/app.js
@@ -5,6 +5,9 @@ function App() {
     return (
         <React.StrictMode>
             <Home/>
+            <audio id="audio" loop autoPlay> 
+                <source src={process.env.PUBLIC_URL + "/bg.mp3"} type="audio/mpeg"/>
+            </audio>
         </React.StrictMode>
     )
 }
