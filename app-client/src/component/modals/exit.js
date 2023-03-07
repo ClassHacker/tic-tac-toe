ef7cc6c6a65f6ff26acded7282c278f71f1debdd
classhacker <ajaydayma1401@gmail.com>
2023-03-07 14:47:29 +0530

    add sound in exit modal

--- app-client/src/component/modals/exit.js
+++ app-client/src/component/modals/exit.js
@@ -1,8 +1,20 @@
 import { Button, Modal, ModalBody, ModalFooter } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { exAction } from '../../redux/actions';
+import { playSound } from '../../utils/sound';
 
 export function ExitModal(props) {
+  function stay() {
+    playSound('b2');
+    dispatch(exAction());
+  }
+
+  function leave() {
+    playSound('b2');
+    dispatch(exAction());
+    exitGame();
+  }
+
   const { exitGame } = props;
   const exR = useSelector(state => state.exR)
   const dispatch = useDispatch()
@@ -15,10 +27,10 @@ export function ExitModal(props) {
         <span className="status2">Do you really want to exit the game?</span>
       </ModalBody>
       <ModalFooter className="custom-modal-footer">
-        <Button variant="primary" className="min-w" onClick={() => {dispatch(exAction()); exitGame()}}>
+        <Button variant="primary" className="min-w" onClick={() => leave()}>
           Yes
         </Button>
-        <Button variant="primary" className="min-w" onClick={() => dispatch(exAction())}>
+        <Button variant="primary" className="min-w" onClick={() => stay()}>
           No
         </Button>
       </ModalFooter>
