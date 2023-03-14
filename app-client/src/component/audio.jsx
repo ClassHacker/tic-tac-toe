7f0748f2300fae9531cd569e630dc14ca7e986ce
classhacker <ajaydayma1401@gmail.com>
2023-03-14 20:40:54 +0530

    add audio element for w1

--- app-client/src/component/audio.jsx
+++ app-client/src/component/audio.jsx
@@ -18,6 +18,9 @@ function Audio() {
             <audio id="c2"> 
                 <source src={process.env.PUBLIC_URL + "/music/c2.mp3"} type="audio/mpeg"/>
             </audio>
+            <audio id="w1"> 
+                <source src={process.env.PUBLIC_URL + "/music/w1.mp3"} type="audio/mpeg"/>
+            </audio>
         </>
     )
 }
