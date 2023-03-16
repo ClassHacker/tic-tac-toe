86c061c26c2c147b303ef3ba8838bf57f183ec62
classhacker <ajaydayma1401@gmail.com>
2023-03-16 21:21:39 +0530

    revert socket.io client changes

--- app-client/src/app.js
+++ app-client/src/app.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import Home from './component/home';
 import Audio from './component/audio';
-import { io } from 'socket.io-client'
 
 function App() {
-    const socket = io("http://localhost:8080")
-    console.log('socket', socket);
     return (
         <React.StrictMode>
             <Home/>
