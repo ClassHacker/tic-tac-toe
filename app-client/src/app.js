925f4a9be7028fda84f8589567264c4f2c5eebfc
classhacker <ajaydayma1401@gmail.com>
2023-03-16 17:30:49 +0530

    add socket.io client

--- app-client/src/app.js
+++ app-client/src/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Home from './component/home';
 import Audio from './component/audio';
+import { io } from 'socket.io-client'
 
 function App() {
+    const socket = io("http://localhost:3000")
     return (
         <React.StrictMode>
             <Home/>
