5bded59841e2e931c063fa5c0f855ef4c5ecabc5
classhacker <ajaydayma1401@gmail.com>
2023-03-23 14:46:41 +0530

    update server

--- server/src/server.js
+++ server/src/server.js
@@ -5,17 +5,25 @@ const io = require("socket.io")(8080, {
   }
 });
 
+const players = new Map();
+
 io.on('connection', socket => {
-  let username = "";
   console.log(socket.id, "connected");
+
   socket.on('register', user => {
     console.log('Setting username to:', user);
-    username = user;
+    if(players.has(user)) {
+      socket.emit('failed', `An user is already present with username: ${user}`);
+    } else {
+      players.set(user, socket.id);
+    }
   });
+
   socket.on('move', i => {
     console.log('received move index: ', i);
     socket.broadcast.emit('move', i);
   });
+
 });
 
 instrument(io, {
