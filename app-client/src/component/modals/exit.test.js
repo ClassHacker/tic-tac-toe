a4d4561e8d73615a6118aa084ebe87bb2e9675a7
classhacker <ajaydayma1401@gmail.com>
2023-03-31 21:42:50 +0530

    lint: update config for jest

--- app-client/src/component/modals/exit.test.js
+++ app-client/src/component/modals/exit.test.js
@@ -1,25 +1,25 @@
-import { screen, fireEvent } from "@testing-library/react";
-import { ExitModal } from "./exit";
-import { renderWithProviders } from "../../utils/test/test-utils";
+import { screen, fireEvent } from '@testing-library/react';
+import { ExitModal } from './exit';
+import { renderWithProviders } from '../../utils/test/test-utils';
 
 jest.mock('../../utils/sound');
 
-describe('test exit modal', ()=> {
-    it('should render exitModal', () => {
-        renderWithProviders(<ExitModal />, { preloadedState: {exR: true} });
-        const modalBodyText = screen.getByText(/Do you really want to exit the game?/i);
-        expect(modalBodyText).toBeInTheDocument();
-    })
-    it('should exit game on Yes', () => {
-        const exitFunc = jest.fn()
-        renderWithProviders(<ExitModal exitGame={exitFunc}/>, { preloadedState: {exR: true} });
-        fireEvent.click(screen.getByText(/Yes/i));
-        expect(exitFunc).toHaveBeenCalledTimes(1);
-    })
-    it('should not exit game on No', () => {
-        const exitFunc = jest.fn()
-        renderWithProviders(<ExitModal exitGame={exitFunc}/>, { preloadedState: {exR: true} });
-        fireEvent.click(screen.getByText(/No/i));
-        expect(exitFunc).toHaveBeenCalledTimes(0);
-    })
+describe('test exit modal', () => {
+  it('should render exitModal', () => {
+    renderWithProviders(<ExitModal />, { preloadedState: { exR: true } });
+    const modalBodyText = screen.getByText(/Do you really want to exit the game?/i);
+    expect(modalBodyText).toBeInTheDocument();
+  })
+  it('should exit game on Yes', () => {
+    const exitFunc = jest.fn()
+    renderWithProviders(<ExitModal exitGame={exitFunc}/>, { preloadedState: { exR: true } });
+    fireEvent.click(screen.getByText(/Yes/i));
+    expect(exitFunc).toHaveBeenCalledTimes(1);
+  })
+  it('should not exit game on No', () => {
+    const exitFunc = jest.fn()
+    renderWithProviders(<ExitModal exitGame={exitFunc}/>, { preloadedState: { exR: true } });
+    fireEvent.click(screen.getByText(/No/i));
+    expect(exitFunc).toHaveBeenCalledTimes(0);
+  })
 })
