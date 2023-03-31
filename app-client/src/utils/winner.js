6159cf035c21c982b92b7669a11931850d1906f9
classhacker <ajaydayma1401@gmail.com>
2023-03-31 21:50:24 +0530

    lint: error fix

--- app-client/src/utils/winner.js
+++ app-client/src/utils/winner.js
@@ -1,21 +1,21 @@
-function calculateWinner(squares, players){
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a] === 'X' ? players[0].name : players[1].name;
-      }
+function calculateWinner (squares, players) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a] === 'X' ? players[0].name : players[1].name;
     }
-    return null;
+  }
+  return null;
 }
 
 export default calculateWinner;
