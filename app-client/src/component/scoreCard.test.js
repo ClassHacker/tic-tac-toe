1c79cf34877abd6dc5b338112b658211958cdd0e
classhacker <ajaydayma1401@gmail.com>
2023-04-03 18:46:41 +0530

    lint: error fix

--- app-client/src/component/scoreCard.test.js
+++ app-client/src/component/scoreCard.test.js
@@ -1,17 +1,17 @@
-import { screen, render } from "@testing-library/react";
-import { ScoreCard } from "./scoreCard";
+import { screen, render } from '@testing-library/react';
+import { ScoreCard } from './scoreCard';
 
 describe('test score card', () => {
-    const players = [{name:'user', gamesWon: 2},{name:'computer', gamesWon: 1}];
-    beforeEach(() => {
-        render(<ScoreCard players={players} />);
-    })
-    it("should render user's score", () => {
-        const userScore = screen.queryByText(/user : 2/i)
-        expect(userScore).toBeTruthy();
-    })
-    it("should render computer's score", () => {
-        const computerScore = screen.queryByText(/user : 2/i)
-        expect(computerScore).toBeTruthy();
-    })
+  const players = [{ name: 'user', gamesWon: 2 }, { name: 'computer', gamesWon: 1 }];
+  beforeEach(() => {
+    render(<ScoreCard players={players} />);
+  })
+  it("should render user's score", () => {
+    const userScore = screen.queryByText(/user : 2/i)
+    expect(userScore).toBeTruthy();
+  })
+  it("should render computer's score", () => {
+    const computerScore = screen.queryByText(/user : 2/i)
+    expect(computerScore).toBeTruthy();
+  })
 })
--- app-client/src/component/square.test.js
+++ app-client/src/component/square.test.js
@@ -1,11 +1,11 @@
-import { screen, render , fireEvent} from "@testing-library/react";
-import Square from "./square";
+import { screen, render, fireEvent } from '@testing-library/react';
+import Square from './square';
 
-describe("test square", () => {
-    it('should render square', () => {
-        const onClickMock = jest.fn();
-        render(<Square onClick={onClickMock} value={0}/>);
-        fireEvent.click(screen.getByText(0));
-        expect(onClickMock).toHaveBeenCalledTimes(1);
-    })
+describe('test square', () => {
+  it('should render square', () => {
+    const onClickMock = jest.fn();
+    render(<Square onClick={onClickMock} value={0}/>);
+    fireEvent.click(screen.getByText(0));
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  })
 })
