bad4766116362dee0df8cd2e3c814713fe5fd7ab
classhacker <ajaydayma1401@gmail.com>
2023-04-03 18:50:12 +0530

    lint: error fix (babel)

--- app-client/babel.config.js
+++ app-client/babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    presets: [
-        '@babel/preset-env', 
-        [
-            "@babel/preset-react", {
-                "runtime": "automatic"
-            }
-        ]
+  presets: [
+    '@babel/preset-env',
+    [
+      '@babel/preset-react', {
+        runtime: 'automatic'
+      }
     ]
+  ]
 }
