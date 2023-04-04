3b4755f84b24edf149e842f3f6c74275a31659d7
classhacker <ajaydayma1401@gmail.com>
2023-04-04 19:25:04 +0530

    lint: error fix

--- app-client/src/utils/computer/hard.js
+++ app-client/src/utils/computer/hard.js
@@ -1,20 +1,20 @@
-import { SuperComputer } from "./superComputer";
+import { SuperComputer } from './superComputer';
 
 export class Computer extends SuperComputer {
-    makeFourthMove(squares, ind) {
-        this.X[3] = ind;
-        let indexes = [];
-        for(let i = 0; i < 9; i++) {
-            if (!(this.X.includes(i) || this.O.includes(i))) {
-                indexes.push(i);
-            }
-        }
-        this.O[3] = this.getLastMoveIndex(indexes);
-        for(let i = 0; i < 8; i++) {
-            indexes.push(this.O[3]);
-        }
-        this.O[3] = this.getRandom(indexes);
-        console.log('fourth move index:', this.O[3]);
-        return this.O[3];
+  makeFourthMove (squares, ind) {
+    this.X[3] = ind;
+    const indexes = [];
+    for (let i = 0; i < 9; i++) {
+      if (!(this.X.includes(i) || this.O.includes(i))) {
+        indexes.push(i);
+      }
     }
+    this.O[3] = this.getLastMoveIndex(indexes);
+    for (let i = 0; i < 8; i++) {
+      indexes.push(this.O[3]);
+    }
+    this.O[3] = this.getRandom(indexes);
+    console.log('fourth move index:', this.O[3]);
+    return this.O[3];
+  }
 }
