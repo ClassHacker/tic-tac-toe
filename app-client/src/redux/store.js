24994ee692418fb224a145f9d8029df978a67b07
classhacker <ajaydayma1401@gmail.com>
2023-04-06 22:16:25 +0530

    lint: error fix

--- app-client/src/redux/store.js
+++ app-client/src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import reducer from "./reducers";
+import reducer from './reducers';
 
-const store = configureStore({reducer: reducer});
+const store = configureStore({ reducer });
 store.subscribe(() => console.log(store.getState()));
 
 export default store;
