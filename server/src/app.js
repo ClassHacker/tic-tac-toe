09da51209b85ab7d93105f653b040a8c3bcad4f3
classhacker <ajaydayma1401@gmail.com>
2023-04-14 18:42:54 +0530

    eslint: error fix

--- server/src/app.js
+++ server/src/app.js
@@ -7,27 +7,27 @@ const Router = require('./routes');
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, "../../app-client/build")));
-app.use(express.json())
+app.use(express.static(path.resolve(__dirname, '../../app-client/build')));
+app.use(express.json());
 mongoose.connect('mongodb+srv://classhacker:<pw>@tchat.c3lxk1k.mongodb.net/mchat',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
 );
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+  console.log('Connected successfully');
 });
 
 app.get('/', async (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../app-client/build', 'index.html'));
-})
+});
 
 app.use(Router);
 
 app.listen(8081, () => {
-  console.log("Server is running at port 8081");
+  console.log('Server is running at port 8081');
 });
