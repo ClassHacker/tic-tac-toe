002d4d6f4ecb06ed670075fa22ab9248aaef3377
classhacker <ajaydayma1401@gmail.com>
2023-04-15 11:42:30 +0530

    eslint: error fix

--- server/src/models.js
+++ server/src/models.js
@@ -1,33 +1,33 @@
 const mongoose = require('mongoose');
 
 const PlayerSchema = new mongoose.Schema({
-    _id: Number,
-    name: {
-        type: String,
-        required: "Player name can't be null or empty."
-    },
-    gamesWon : {
-        type: Number,
-        min: 0,
-        required: true,
-    },
-    // gamesPlayed : {
-    //     type: Number,
-    //     min: 0,
-    //     required: true,
-    //     validate: {
-    //         validator: (value) => {
-    //             return value >= this.gamesWon;
-    //         },
-    //         message: (props) => `gamesPlayed can't be less than gamesWon, invalid value: ${props.value}`
-    //     }
-    // },
-})
+  _id: Number,
+  name: {
+    type: String,
+    required: 'Player name can\'t be null or empty.',
+  },
+  gamesWon: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  // gamesPlayed : {
+  //     type: Number,
+  //     min: 0,
+  //     required: true,
+  //     validate: {
+  //         validator: (value) => {
+  //             return value >= this.gamesWon;
+  //         },
+  //         message: (props) => `gamesPlayed can't be less than gamesWon, invalid value: ${props.value}`
+  //     }
+  // },
+});
 
 // PlayerSchema.path('gamesWon').validate((value)=>{
 //     return value >= this.gamesPlayed;
 // })
 
-const Player = mongoose.model("Player",PlayerSchema);
+const Player = mongoose.model('Player', PlayerSchema);
 
-module.exports = Player
+module.exports = Player;
